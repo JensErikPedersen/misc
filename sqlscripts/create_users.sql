4c3e5c3adac092bc08d63bbebbd093ff1bb90bf2
EKY <eky@topdanmark.dk>
2017-05-11 14:35:05 +0200

    Adjusted create user script

--- sqlscripts/create_users.sql
+++ sqlscripts/create_users.sql
@@ -1,14 +1,23 @@
 CREATE USER 'events_user' IDENTIFIED BY 'events123';
-
-CREATE USER 'events_admin' IDENTIFIED BY 'events123';
+CREATE USER 'events_manager' IDENTIFIED BY 'events123';
 
 GRANT SELECT ON eventmanagement.* TO 'events_user';
 
-GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.persons TO 'events_admin';
+GRANT SELECT, INSERT, DELETE, UPDATE, EXECUTE ON eventmanagement.* TO 'events_manager';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.events TO 'events_user';
+GRANT SELECT ON eventmanagement.rooms TO 'events_user';
+GRANT SELECT ON eventmanagement.event_types TO 'events_user';
+GRANT SELECT ON eventmanagement.persons TO 'events_user';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.events_persons TO 'events_user';
+
+
+
+/*
 GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.rooms TO 'events_admin';
 GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.logs TO 'events_admin';
 GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.event_types TO 'events_admin';
-GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.events TO 'events_admin', 'events_user';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.events_persons TO 'events_admin';
+
 
 USE eventmanagement;
 
@@ -18,4 +27,6 @@ USE mysql;
 
 SELECT * FROM user;
 
-DROP USER 'events_user', 'events_admin';
+DROP USER 'events_user', 'events_manager';
+*/
+
