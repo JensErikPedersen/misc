9739e01f1c56795fcf39ac0248daae38e1050203
EKY <eky@topdanmark.dk>
2017-05-11 12:37:09 +0200

    Modified create_users script

--- sqlscripts/create_users.sql
+++ sqlscripts/create_users.sql
@@ -2,13 +2,15 @@ CREATE USER 'events_user' IDENTIFIED BY 'events123';
 
 CREATE USER 'events_admin' IDENTIFIED BY 'events123';
 
-GRANT SELECT ON eventmanagement.* TO 'events_user', 'events_admin';
+GRANT SELECT ON eventmanagement.* TO 'events_user';
 
-GRANT INSERT, DELETE, UPDATE ON eventmanagement.persons TO 'events_admin';
-GRANT INSERT, DELETE, UPDATE ON eventmanagement.rooms TO 'events_admin';
-GRANT INSERT, DELETE, UPDATE ON eventmanagement.logs TO 'events_admin';
-GRANT INSERT, DELETE, UPDATE ON eventmanagement.event_types TO 'events_admin';
-GRANT INSERT, DELETE, UPDATE ON eventmanagement.events TO 'events_admin', 'events_user';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.persons TO 'events_admin';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.rooms TO 'events_admin';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.logs TO 'events_admin';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.event_types TO 'events_admin';
+GRANT SELECT, INSERT, DELETE, UPDATE ON eventmanagement.events TO 'events_admin', 'events_user';
+
+USE eventmanagement;
 
 SHOW GRANTS FOR 'events_user';
 
@@ -16,4 +18,4 @@ USE mysql;
 
 SELECT * FROM user;
 
-DROP USER 'events_read', 'events_write', 'events_admin';
+DROP USER 'events_user', 'events_admin';
